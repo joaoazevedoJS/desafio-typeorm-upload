b57b0528594ffee8d6033b934251ba86dc4fc59c
Joao Azevedo <devjoaoazevedo@gmail.com>
2020-12-26 12:51:04 -0300

    Finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,31 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const balance = transactions.reduce(
+      (value, transaction) => {
+        const balanceValue = value;
+        const valueTransaction = Number(transaction.value);
+
+        if (transaction.type === 'income') {
+          balanceValue.income += valueTransaction;
+          balanceValue.total += valueTransaction;
+        } else {
+          balanceValue.outcome += valueTransaction;
+          balanceValue.total -= valueTransaction;
+        }
+
+        return balanceValue;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 }
 
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,22 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepositories = getCustomRepository(
+      TransactionsRepository,
+    );
+
+    const transaction = await transactionsRepositories.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction not found', 404);
+    }
+
+    await transactionsRepositories.delete(transaction.id);
   }
 }
 
